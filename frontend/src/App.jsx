20f33c3ebbc4809c27a70d98a26a6d1c58373227
Devanand Puzhakkool <132560246+CodeByD3v@users.noreply.github.com>
2026-06-25 10:02:05 +0530

    feat: wrap App in AuthProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import Navbar from './components/Navbar'
 import Dashboard from './pages/Dashboard'
 import Report from './pages/Report'
@@ -7,16 +8,18 @@ import Leaderboard from './pages/Leaderboard'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <main style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/report" element={<Report />} />
-          <Route path="/issue/:id" element={<IssueDetail />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <main style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+          <Routes>
+            <Route path="/"           element={<Dashboard />} />
+            <Route path="/report"     element={<Report />} />
+            <Route path="/issue/:id"  element={<IssueDetail />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }

9d8c1b7bb9261d9fe3e53f76878bd10676afd321
Devanand Puzhakkool <132560246+CodeByD3v@users.noreply.github.com>
2026-06-29 15:04:04 +0530

    fix: register PWA service worker on startup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
 import './index.css'
+
+registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() { console.log('[PWA] Ready to work offline') },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
